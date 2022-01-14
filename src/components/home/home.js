a522ae1c5c5bf67d80bba60ea2e34960efa19cdd
katerz avocado <ktrinaapodaca@yahoo.com>
2022-01-14 16:07:32 -0700

    adding button on homepage

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,37 +1,35 @@
 import { text } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 // import background from "../../../1183560.jpg"
+import { Button } from "react-bootstrap"
 import "../aboutme/aboutme.css";
 
 const home = () => {
   return (
     <div>
-   
-     
-     
       <div style={{ backgroundColor: "black", textAlign: "center" }}>
         <img
           src="../../../1183560.jpg"
           className="evangelion"
           style={{ overflow: "hidden", width: "52%", textAlign: "right" }}
-          ></img>
+        ></img>
         <main className="maininfo">
-           <p
-        style={{
-          backgroundColor: "black",
-          textAlign: "center",
-          color: "white",
-          fontFamily: "'Orbitron', sans-serif",
-          bottom: "25%",
-          fontSize: "20px"
-        }}
-      >
-        Greetings! My name is K'trina Apodaca. I am a Full Stack Jr Dev and you are about to explore my portfolio designed to show my expertise and personality. Any questions, thoughts, or feedback feel free to reach out to me!
-    
-        </p>
+          <p
+            style={{
+              backgroundColor: "black",
+              textAlign: "center",
+              color: "white",
+              fontFamily: "'Orbitron', sans-serif",
+              bottom: "25%",
+              fontSize: "20px",
+            }}
+          >
+            <br></br>
+            <Button variant="link"
+            style={{color:"white"}}>E N T E R</Button>
+          </p>
         </main>
       </div>
-         
     </div>
   );
 };
